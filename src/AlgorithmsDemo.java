c9fc64775387956fdc92597fff5a0f08eb2f1543
Abhishek Saseendran <abhisheksaseendran@Abhisheks-MacBook-Pro.local>
2020-05-03 20:09:57 +0530

    Added Bubble Sort

--- src/AlgorithmsDemo.java
+++ src/AlgorithmsDemo.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import com.thegyrfalcon.gandalf.algorithms.sort.BubbleSort;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class AlgorithmsDemo {
+
+    public static void main(String[] args) {
+        System.out.println("*************** Algorithms Demo ***************");
+
+        System.out.println("---------- START: Search Algorithms ----------");
+        System.out.println("---------- END:   Search Algorithms ----------");
+
+        System.out.println("\n---------- START: Sort Algorithms ----------\n");
+
+        // Bubble Sort
+        System.out.println("\n---------- Bubble Sort ----------\n");
+        int[] values = { 25, 9, 33, -30, 50, 5 };
+        System.out.println("Before Sort :"+ Arrays.toString(values));
+        BubbleSort.sort(values);
+        System.out.println("After Sort  :"+ Arrays.toString(values));
+
+        System.out.println("---------- END:   Sort Algorithms ----------");
+    }
 }
