fa4271bb65293a6ba89a3923f8e7b2ea2d55f7ed
Abhishek Saseendran <abhisheksaseendran@Abhisheks-MacBook-Pro.local>
2020-05-03 21:45:25 +0530

    02.Selection Sort

--- src/com/thegyrfalcon/gandalf/algorithms/sort/BubbleSort.java
+++ src/com/thegyrfalcon/gandalf/algorithms/sort/BubbleSort.java
@@ -13,7 +13,7 @@ public class BubbleSort {
      * @return arr
      */
     public static int[] sort(int[] arr) {
-        for (int i = 0; i < arr.length - 1; i++) {
+        for (int i = 0; i < arr.length; i++) {
             for (int j = 0; j < (arr.length -1 -i); j++) {
                 if(arr[j] > arr[j+1]) {
                     int lesser = arr[j + 1];
